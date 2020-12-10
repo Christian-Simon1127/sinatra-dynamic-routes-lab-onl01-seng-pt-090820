738cbb9cff3b9e1b8692f582631472007eb7d295
Christian Simon <csimon1127@gmail.com>
2020-12-10 19:35:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,6 +15,9 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do 
     @say = params[:phrase]
     @times = params[:number].to_i
+    @times.times do 
+      @say += " " + params[:phrase]
+    end
   end
   
   
